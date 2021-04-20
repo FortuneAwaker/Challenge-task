f2edc6a8a5b0f8e463bfd8298622894eb3f6e1f5
Alexander Popovich <oleshapopovich2000@gmail.com>
2021-04-20 23:14:55 +0300

    Code cleaning.

--- consumer/src/main/resources/data.sql
+++ consumer/src/main/resources/data.sql
@@ -30,17 +30,3 @@ ALTER TABLE trip_waypoint
         REFERENCES booking (id)
         ON UPDATE CASCADE
         ON DELETE RESTRICT;
-
-
-INSERT INTO booking(passenger_name, passenger_contact_number, pickup_time, asap, waiting_time, number_of_passengers,
-                    price, rating, created_on, last_modified_on)
-VALUES ('Alex', '123456789123', '10:00', true, '10:30', 15, 30.5, 9.7, '2021-05-12', '2021-05-12'),
-       ('Happy Tourist', '098765432156', '12:00', true, '12:30', 4, 27, 9.2, '2021-05-13', '2021-05-13');
-
-INSERT INTO trip_waypoint(locality, latitude, longitude, booking_id)
-VALUES ('Minsk', 53.893009, 27.567444, 1),
-       ('Brest', 52.097622, 23.734051, 1),
-       ('Grodno', 53.669353, 23.813131, 1),
-       ('Vitebsk', 55.187222, 30.205116, 2),
-       ('Mogilev', 53.900716, 30.33136, 2),
-       ('Gomel', 52.4411761, 30.9878462, 2);
